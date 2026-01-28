637fab77f63ee8197296ce17a39011d63b10106c
Code with Riya <riya.2024.1160@mirandahouse.ac.in>
2026-01-28 07:02:29 -0800

    Create factorial.cpp

---
+++ C++/factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+int fact=1;
+for(int i=1;i<=n;i++){
+fact*=i;
+}return fact;
+}
+int main(){
+  int n;
+  cout<<"Enter the number";
+  cin>>n;
+  cout<<"The factorial of "<<n<<"is "<<fact(n);
+}
