c964603d512d9b450711d798010d8d008b483af4
Code with Riya <riya.2024.1160@mirandahouse.ac.in>
2026-01-28 06:37:07 -0800

    Create Reverse_number.cpp

---
+++ C++/Reverse_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number";
+    cin>>n;
+    int reverse=0;
+    int lastdigit=0;
+    while(n>0){
+    reverse*=10;
+    lastdigit=n%10;
+    reverse+=lastdigit;
+    n/=10;
+ } cout<<reverse;
+}
