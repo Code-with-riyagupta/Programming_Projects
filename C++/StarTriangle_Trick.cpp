34241763361dfbb639f6f138f8fb99bcfa791386
Code with Riya <riya.2024.1160@mirandahouse.ac.in>
2026-01-28 06:47:00 -0800

    Fix typo in comment about starTriangle function
    Corrected spelling of 'Triangles' in comments.

--- C++/StarTriangle_Trick.cpp
+++ C++/StarTriangle_Trick.cpp
@@ -1,5 +1,5 @@
 // Here I wanna show you the wonderful advantage of using functions that is without writing codes multiple times for repeating star triangle pattern of different sizes,
-// there is a function named starTriangle by which we can make any number of Traingles of any size by just calling it.
+// there is a function named starTriangle by which we can make any number of Triangles of any size by just calling it.
 
 #include<iostream>
 using namespace std;
